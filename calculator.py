bb35f81773a778b2dd763edb8afdfb5cbc33b735
Larchenko Ivan <vanyalarchenko@gmail.com>
2026-03-27 03:57:08 +0300

    Initial commit: add subtract function

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 def add(a, b):
 	return a + b
+def subtract(a, b):
+        return a - b
 if __name__ == "__main__":
 	print(f"Сумма 5 и 3 равна {add(5, 3)}")
+	print(f"Разность 5 и 3 = {subtract(5, 3)}")
